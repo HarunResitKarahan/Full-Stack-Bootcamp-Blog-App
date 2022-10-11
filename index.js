2359fb6e89583f5ab0b44aeb27c59db9d1611e43
Harun <harun_artin@hotmail.com>
2022-10-11 23:30:47 +0300

    .

--- index.js
+++ index.js
@@ -10,3 +10,12 @@ function changeLanguage() {
         languageIcon.src = './assets/images/turkey.png'
     }
 }
+function displayColumn() {
+    document.querySelector('.displayRow').style.display = 'none'
+    document.querySelector('.displayColumn').style.display = 'block'
+}
+function displayRow() {
+    document.querySelector('.displayRow').style.display = 'block'
+    document.querySelector('.displayColumn').style.display = 'none'
+
+}
