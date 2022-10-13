746d10b55c23954ad4ad7016d92884bd7a2a76e0
Harun <harun_artin@hotmail.com>
2022-10-13 14:30:20 +0300

    .

--- index.js
+++ index.js
@@ -23,10 +23,12 @@ function changeLanguage() {
         languageIcon.src = './assets/images/turkey.png'
     }
 }
+
 function displayColumn() {
     document.querySelector('.displayRow').style.display = 'none'
     document.querySelector('.displayColumn').style.display = 'block'
 }
+
 function displayRow() {
     document.querySelector('.displayRow').style.display = 'block'
     document.querySelector('.displayColumn').style.display = 'none'
